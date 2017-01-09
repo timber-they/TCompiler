78e6275179ea4a701ce60067646d570ac8e98a65
Timo Borner <timo@borners.de>
2017-01-09 23:13:21 +0100

    Documentation

--- TCompiler/Types/CompilingTypes/Block/ElseBlock.cs
+++ TCompiler/Types/CompilingTypes/Block/ElseBlock.cs
@@ -1,12 +1,24 @@
 ﻿namespace TCompiler.Types.CompilingTypes.Block
 {
+    /// <summary>
+    /// The block for an else statement. 
+    /// This must beginn in an if block, while it appears to be the end of the if block, so that the endif is actually the end of the else block then.
+    /// </summary>
     public class ElseBlock : Block
     {
+        /// <summary>
+        /// Initializes a new ElseBlock
+        /// </summary>
+        /// <param name="endLabel">The end label of the else (/if) block</param>
+        /// <param name="elseLabel">The start of the else block</param>
         public ElseBlock(Label endLabel, Label elseLabel) : base(endLabel)
         {
             ElseLabel = elseLabel;
         }
 
+        /// <summary>
+        /// The start of the else block
+        /// </summary>
         public Label ElseLabel { get; }
     }
 }
